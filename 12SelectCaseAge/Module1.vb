ffb90bc8679641c4877033ae61c4d1e73b8236ea
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-07 13:49:55 +0530

    Add files via upload

---
+++ 12SelectCaseAge/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim age As Integer
+
+        Console.Write("Enter Age:")
+        age = Console.ReadLine()
+
+        Console.WriteLine()
+
+        Select Case age
+            Case 1 To 6
+                Console.WriteLine("Kid")
+            Case 7 To 18
+                Console.WriteLine("Teenage")
+            Case 19 To 100
+                Console.WriteLine("Adult")
+        End Select
+        Console.ReadKey()
+    End Sub
+
+End Module
