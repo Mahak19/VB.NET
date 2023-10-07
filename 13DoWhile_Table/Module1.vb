e44a85f4726546cad12f728221ff073625ef5a91
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-07 13:50:39 +0530

    Add files via upload

---
+++ 13DoWhile_Table/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim i, n As Byte
+        Console.Write("Enter a number: ")
+        n = Console.ReadLine()
+
+        Console.WriteLine()
+        Console.Write("Table of " & n & " is: ")
+        Console.WriteLine()
+        Console.WriteLine()
+
+
+        i = 1
+        Do While i <= 10
+            Console.WriteLine(n & "x" & i & "=" & n * i)
+            i = i + 1
+        Loop
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ 13DoWhile_Table/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("13DoWhile_Table")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("13DoWhile_Table")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("20b466ac-c69c-4627-bb79-e1291485e5d6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
