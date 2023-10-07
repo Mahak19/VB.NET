d15ed1307fdb20f95c57e89d4bec7f0ef0c9c799
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-07 13:34:24 +0530

    Add files via upload

---
+++ 1SimpleProg_Forms/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmSimple
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnOk = New Button()
+        SuspendLayout()
+        ' 
+        ' btnOk
+        ' 
+        btnOk.Location = New Point(68, 55)
+        btnOk.Name = "btnOk"
+        btnOk.Size = New Size(138, 45)
+        btnOk.TabIndex = 0
+        btnOk.Text = "Ok"
+        btnOk.UseVisualStyleBackColor = True
+        ' 
+        ' frmSimple
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(284, 153)
+        Controls.Add(btnOk)
+        Name = "frmSimple"
+        Text = "SimpleProgram"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnOk As Button
+End Class
---
+++ 1SimpleProg_Forms/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class frmSimple
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        MsgBox("Welcome in VB.Net")
+    End Sub
+End Class
