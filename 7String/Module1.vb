22a91f5dc1159d491e7fbdcb3b87d46dbda5b319
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-07 13:44:01 +0530

    Add files via upload

---
+++ 7String/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s As String
+        Dim r As Boolean
+
+        s = "Hello"
+        r = s Like "h??l?"
+        Console.Write("The string start with s is " & r)
+        Console.ReadKey()
+    End Sub
+
+End Module
