6b0f0911cf7ffc47c161cb16229b8b6310ea1029
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-07 13:47:53 +0530

    Add files via upload

---
+++ 10IIf_Max/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a, b, max As Integer
+        Console.Write("Enter Value of A: ")
+        a = Console.ReadLine()
+
+        Console.Write("Enter Value of B:")
+        b = Console.ReadLine
+
+        max = IIf(a > b, a, b)
+        Console.WriteLine("Maximum Number is: " & max)
+        Console.ReadKey()
+    End Sub
+
+End Module
