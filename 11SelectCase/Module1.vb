585fad7f2023393b398de557d7f11200c8f83ab1
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-07 13:49:06 +0530

    Add files via upload

---
+++ 11SelectCase/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a, b, c As Double
+
+        Console.Write("Enter Value of A: ")
+        a = Console.ReadLine()
+        Console.Write("Enter Value of B: ")
+        b = Console.ReadLine()
+
+        Dim op As String
+        Console.Write("Enter Operator: ")
+        op = Console.ReadLine()
+        Select Case op
+            Case "+"
+                c = a + b
+                Console.WriteLine("Addition is:" & c)
+            Case "-"
+                c = a - b
+                Console.WriteLine("Subtraction is:" & c)
+            Case "*"
+                c = a + b
+                Console.WriteLine("Multiplication is:" & c)
+            Case "/"
+                c = a Mod b
+                Console.WriteLine("Division is:" & c)
+            Case Else
+                Console.WriteLine("Wrong")
+        End Select
+        Console.ReadKey()
+    End Sub
+
+End Module
