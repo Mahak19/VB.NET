5a5760633703c6161a768bd428f38e9ade6f05f0
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-15 08:28:32 +0530

    Add files via upload

---
+++ 21Array/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim ar As Integer() = {20, 22, 23, 24, 26}
+        Dim i As Object
+        Console.Write("Value of array: ")
+        Console.WriteLine()
+        For Each i In ar
+            Console.WriteLine(i)
+        Next
+        Console.WriteLine()
+        Console.WriteLine("Number of elements in array: " & ar.Length)
+
+        Console.WriteLine()
+
+        Console.ReadKey()
+    End Sub
+
+End Module
