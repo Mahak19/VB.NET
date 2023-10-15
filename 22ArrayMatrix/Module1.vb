4fcd8e7d6087075ca81ce38591cd8eafbd25e005
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-15 08:29:09 +0530

    Add files via upload

---
+++ 22ArrayMatrix/Module1.vb
@@ -0,0 +1,25 @@
+﻿
+Module Module1
+
+    Sub Main()
+        Dim Arr(2, 2) As Integer
+        Dim i, j As Integer
+        For i = 0 To 2
+            For j = 0 To 2
+                Console.Write("Enter Value: ")
+                Arr(i, j) = Console.ReadLine()
+            Next
+            Console.WriteLine()
+        Next
+        Console.WriteLine("Values are in Matrix form: ")
+        For i = 0 To 2
+            For j = 0 To 2
+                Console.Write(Arr(i, j) & " ")
+            Next
+            Console.WriteLine()
+        Next
+        Console.ReadKey()
+
+    End Sub
+
+End Module
