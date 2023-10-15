427436b90850d67aed3c3e9c7b0727dcc74c137d
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-15 08:22:50 +0530

    Add files via upload

---
+++ 14DoWhile_EvenOdd/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim i, n As Byte
+
+        Console.Write("Enter limit to print even number series: ")
+        n = Console.ReadLine()
+        Console.WriteLine()
+
+        Console.WriteLine("Numbers are: ")
+
+        i = 1
+        Do While i <= n
+            If i Mod 2 = 0 Then
+                Console.WriteLine(i & " ")
+            End If
+            i = i + 1
+        Loop
+        Console.ReadKey()
+    End Sub
+
+End Module
