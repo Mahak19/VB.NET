87d6468f2b05cbb040b9714a33f168c5c4d8ceef
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-15 08:25:50 +0530

    Add files via upload

---
+++ 17DoUntil_Factorial/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n, f, i As Integer
+        f = 1
+        Console.Write("Enter a number: ")
+        n = Console.ReadLine()
+
+        For i = 1 To n
+            f = f * i
+        Next
+        Console.WriteLine("Factorial of " & n & " is " & f)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
