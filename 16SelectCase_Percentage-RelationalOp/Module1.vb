c2691a5dd98423452c8425e4c55bf9d96d6c922c
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-15 08:25:03 +0530

    Add files via upload

---
+++ 16SelectCase_Percentage-RelationalOp/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim percentage As Integer
+        Console.Write("Enter ur percentage: ")
+        percentage = Console.ReadLine()
+
+        Select Case percentage
+            Case Is >= 84
+                Console.WriteLine("Grade A+")
+            Case Is >= 76
+                Console.WriteLine("Grade A")
+            Case Is >= 60
+                Console.WriteLine("Grade B")
+            Case Is >= 54
+                Console.WriteLine("Grade C")
+            Case Is >= 36
+                Console.WriteLine("Grade D")
+            Case Else
+                Console.WriteLine("Better Luck next time")
+        End Select
+        Console.ReadKey()
+    End Sub
+
+End Module
