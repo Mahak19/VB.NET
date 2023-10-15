b523031d7b4b0bf8a66bd33aee85a2cc9b797fda
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-15 08:29:54 +0530

    Add files via upload

---
+++ 23ArrayDiagonal/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim arr(2, 2) As Integer
+        Dim i, j As Byte
+        For i = 0 To 2
+            For j = 0 To 2
+                Console.Write("Enter Value: ")
+                arr(i, j) = Console.ReadLine()
+            Next
+            Console.WriteLine()
+        Next
+        Console.WriteLine("Values are in diagonal form: ")
+        For i = 0 To 2
+            For j = 0 To 2
+                If (i = j) Then
+                    Console.Write(arr(i, j))
+                Else
+                    Console.Write(" ")
+                End If
+            Next
+            Console.WriteLine()
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
