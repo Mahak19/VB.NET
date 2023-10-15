3782ee6311a6f4c47ae9f0c7f28452c019a2109d
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-15 08:27:16 +0530

    Add files via upload

---
+++ 19DynamicArray/Module1.vb
@@ -0,0 +1,22 @@
+﻿
+Module Module1
+
+    Sub Main()
+        Dim D() As Integer
+        Dim s, i As Byte
+        Console.Write("Enter the size of Array: ")
+        s = Console.ReadLine()
+        ReDim D(s)
+        For i = 0 To s
+            Console.Write("Enter values:")
+            D(i) = Console.ReadLine()
+        Next
+        Console.WriteLine()
+        Console.WriteLine("Values are: ")
+        For i = 0 To s
+            Console.WriteLine(D(i))
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
