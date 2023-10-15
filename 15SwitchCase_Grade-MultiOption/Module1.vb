6498404832c8dae3b18e5eae738295b3872fb787
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-10-15 08:23:51 +0530

    Add files via upload

---
+++ 15SwitchCase_Grade-MultiOption/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim grade As Integer
+        Console.Write("Enter grade: ")
+        grade = Console.ReadLine()
+
+        Select Case grade
+            Case 10, 9
+                Console.WriteLine("Excellent")
+            Case 8, 7
+                Console.WriteLine("Very Good")
+            Case 6, 5
+                Console.WriteLine("Good")
+            Case Else
+                Console.WriteLine("Poor")
+        End Select
+        Console.ReadKey()
+    End Sub
+
+End Module
