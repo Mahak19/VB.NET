84dfcf970fb8818a3dc8cf938d69efc23bf9c02b
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-11-04 10:16:56 +0530

    Add files via upload

---
+++ 42ProgressBar/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ProgressBar1 = New ProgressBar()
+        btnStart = New Button()
+        SuspendLayout()
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Location = New Point(33, 62)
+        ProgressBar1.Maximum = 10000
+        ProgressBar1.Minimum = 1
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(434, 34)
+        ProgressBar1.Step = 1
+        ProgressBar1.Style = ProgressBarStyle.Continuous
+        ProgressBar1.TabIndex = 0
+        ProgressBar1.Value = 1
+        ' 
+        ' btnStart
+        ' 
+        btnStart.Location = New Point(194, 142)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(112, 34)
+        btnStart.TabIndex = 1
+        btnStart.Text = "Start"
+        btnStart.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(500, 227)
+        Controls.Add(btnStart)
+        Controls.Add(ProgressBar1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents btnStart As Button
+End Class
---
+++ 42ProgressBar/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Dim i As Integer
+        For i = ProgressBar1.Minimum To ProgressBar1.Maximum - 1
+            ProgressBar1.PerformStep()
+        Next
+    End Sub
+End Class
