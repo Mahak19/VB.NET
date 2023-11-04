8c290f4301edd31d03f8e42c304cbca2ccf524d5
Mahak19 <142742988+Mahak19@users.noreply.github.com>
2023-11-04 10:21:01 +0530

    Add files via upload

---
+++ 45NumberPuzzle/Form1.Designer.vb
@@ -0,0 +1,288 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmNumberPuzzle
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btn1 = New Button()
+        btn2 = New Button()
+        btn3 = New Button()
+        btn4 = New Button()
+        btn5 = New Button()
+        btn6 = New Button()
+        btn7 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btn10 = New Button()
+        btn11 = New Button()
+        btn12 = New Button()
+        btn13 = New Button()
+        btn14 = New Button()
+        btn15 = New Button()
+        btn16 = New Button()
+        btnSubmit = New Button()
+        btnReset = New Button()
+        SuspendLayout()
+        ' 
+        ' btn1
+        ' 
+        btn1.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn1.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn1.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn1.Location = New Point(42, 14)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(60, 63)
+        btn1.TabIndex = 0
+        btn1.UseVisualStyleBackColor = False
+        ' 
+        ' btn2
+        ' 
+        btn2.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn2.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn2.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn2.Location = New Point(127, 14)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(64, 63)
+        btn2.TabIndex = 1
+        btn2.UseVisualStyleBackColor = False
+        ' 
+        ' btn3
+        ' 
+        btn3.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn3.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn3.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn3.Location = New Point(218, 14)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(61, 63)
+        btn3.TabIndex = 2
+        btn3.UseVisualStyleBackColor = False
+        ' 
+        ' btn4
+        ' 
+        btn4.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn4.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn4.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn4.Location = New Point(306, 14)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(63, 63)
+        btn4.TabIndex = 3
+        btn4.UseVisualStyleBackColor = False
+        ' 
+        ' btn5
+        ' 
+        btn5.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn5.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn5.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn5.Location = New Point(42, 83)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(60, 62)
+        btn5.TabIndex = 4
+        btn5.UseVisualStyleBackColor = False
+        ' 
+        ' btn6
+        ' 
+        btn6.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn6.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn6.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn6.Location = New Point(127, 83)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(64, 62)
+        btn6.TabIndex = 5
+        btn6.UseVisualStyleBackColor = False
+        ' 
+        ' btn7
+        ' 
+        btn7.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn7.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn7.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn7.Location = New Point(218, 83)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(61, 62)
+        btn7.TabIndex = 6
+        btn7.UseVisualStyleBackColor = False
+        ' 
+        ' btn8
+        ' 
+        btn8.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn8.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn8.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn8.Location = New Point(306, 83)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(63, 62)
+        btn8.TabIndex = 7
+        btn8.UseVisualStyleBackColor = False
+        ' 
+        ' btn9
+        ' 
+        btn9.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn9.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn9.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn9.Location = New Point(42, 151)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(60, 63)
+        btn9.TabIndex = 8
+        btn9.UseVisualStyleBackColor = False
+        ' 
+        ' btn10
+        ' 
+        btn10.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn10.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn10.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn10.Location = New Point(127, 151)
+        btn10.Name = "btn10"
+        btn10.Size = New Size(64, 63)
+        btn10.TabIndex = 9
+        btn10.UseVisualStyleBackColor = False
+        ' 
+        ' btn11
+        ' 
+        btn11.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn11.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn11.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn11.Location = New Point(218, 151)
+        btn11.Name = "btn11"
+        btn11.Size = New Size(61, 63)
+        btn11.TabIndex = 10
+        btn11.UseVisualStyleBackColor = False
+        ' 
+        ' btn12
+        ' 
+        btn12.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn12.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn12.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn12.Location = New Point(306, 151)
+        btn12.Name = "btn12"
+        btn12.Size = New Size(63, 63)
+        btn12.TabIndex = 11
+        btn12.UseVisualStyleBackColor = False
+        ' 
+        ' btn13
+        ' 
+        btn13.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn13.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn13.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn13.Location = New Point(42, 220)
+        btn13.Name = "btn13"
+        btn13.Size = New Size(60, 61)
+        btn13.TabIndex = 12
+        btn13.UseVisualStyleBackColor = False
+        ' 
+        ' btn14
+        ' 
+        btn14.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn14.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn14.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn14.Location = New Point(127, 220)
+        btn14.Name = "btn14"
+        btn14.Size = New Size(64, 61)
+        btn14.TabIndex = 13
+        btn14.UseVisualStyleBackColor = False
+        ' 
+        ' btn15
+        ' 
+        btn15.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn15.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn15.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn15.Location = New Point(218, 220)
+        btn15.Name = "btn15"
+        btn15.Size = New Size(61, 61)
+        btn15.TabIndex = 14
+        btn15.UseVisualStyleBackColor = False
+        ' 
+        ' btn16
+        ' 
+        btn16.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn16.Font = New Font("MV Boli", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        btn16.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(0))
+        btn16.Location = New Point(306, 220)
+        btn16.Name = "btn16"
+        btn16.Size = New Size(63, 61)
+        btn16.TabIndex = 15
+        btn16.UseVisualStyleBackColor = False
+        ' 
+        ' btnSubmit
+        ' 
+        btnSubmit.Location = New Point(57, 303)
+        btnSubmit.Name = "btnSubmit"
+        btnSubmit.Size = New Size(113, 49)
+        btnSubmit.TabIndex = 16
+        btnSubmit.Text = "Submit"
+        btnSubmit.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(220, 303)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(112, 49)
+        btnReset.TabIndex = 17
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' frmNumberPuzzle
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Lavender
+        ClientSize = New Size(406, 372)
+        Controls.Add(btnReset)
+        Controls.Add(btnSubmit)
+        Controls.Add(btn16)
+        Controls.Add(btn15)
+        Controls.Add(btn14)
+        Controls.Add(btn13)
+        Controls.Add(btn12)
+        Controls.Add(btn11)
+        Controls.Add(btn10)
+        Controls.Add(btn9)
+        Controls.Add(btn8)
+        Controls.Add(btn7)
+        Controls.Add(btn6)
+        Controls.Add(btn5)
+        Controls.Add(btn4)
+        Controls.Add(btn3)
+        Controls.Add(btn2)
+        Controls.Add(btn1)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "frmNumberPuzzle"
+        Text = "Number Puzzle"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn10 As Button
+    Friend WithEvents btn11 As Button
+    Friend WithEvents btn12 As Button
+    Friend WithEvents btn13 As Button
+    Friend WithEvents btn14 As Button
+    Friend WithEvents btn15 As Button
+    Friend WithEvents btn16 As Button
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnReset As Button
+End Class
---
+++ 45NumberPuzzle/Form1.vb
@@ -0,0 +1,282 @@
+﻿Public Class frmNumberPuzzle
+    Private Sub frmNumberPuzzle_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btn1.Text = 2
+        btn2.Text = 4
+        btn3.Text = 1
+        btn4.Text = 3
+        btn5.Text = 6
+        btn6.Text = 5
+        btn7.Text = 9
+        btn8.Text = 7
+        btn9.Text = ""
+        btn10.Text = 15
+        btn11.Text = 14
+        btn12.Text = 11
+        btn13.Text = 10
+        btn14.Text = 12
+        btn15.Text = 8
+        btn16.Text = 13
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        If btn2.Text = "" Then
+            btn2.Text = btn1.Text
+            btn1.Text = ""
+        End If
+        If btn5.Text = "" Then
+            btn5.Text = btn1.Text
+            btn1.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        If btn1.Text = "" Then
+            btn1.Text = btn2.Text
+            btn2.Text = ""
+        End If
+        If btn3.Text = "" Then
+            btn3.Text = btn2.Text
+            btn2.Text = ""
+        End If
+        If btn6.Text = "" Then
+            btn6.Text = btn2.Text
+            btn2.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        If btn2.Text = "" Then
+            btn2.Text = btn3.Text
+            btn3.Text = ""
+        End If
+        If btn4.Text = "" Then
+            btn4.Text = btn3.Text
+            btn3.Text = ""
+        End If
+        If btn7.Text = "" Then
+            btn7.Text = btn3.Text
+            btn3.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        If btn3.Text = "" Then
+            btn3.Text = btn4.Text
+            btn4.Text = ""
+        End If
+        If btn8.Text = "" Then
+            btn8.Text = btn4.Text
+            btn4.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        If btn1.Text = "" Then
+            btn1.Text = btn5.Text
+            btn5.Text = ""
+        End If
+        If btn6.Text = "" Then
+            btn6.Text = btn5.Text
+            btn5.Text = ""
+        End If
+        If btn9.Text = "" Then
+            btn9.Text = btn5.Text
+            btn5.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        If btn2.Text = "" Then
+            btn2.Text = btn6.Text
+            btn6.Text = ""
+        End If
+        If btn5.Text = "" Then
+            btn5.Text = btn6.Text
+            btn6.Text = ""
+        End If
+        If btn7.Text = "" Then
+            btn7.Text = btn6.Text
+            btn6.Text = ""
+        End If
+        If btn10.Text = "" Then
+            btn10.Text = btn6.Text
+            btn6.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        If btn3.Text = "" Then
+            btn3.Text = btn7.Text
+            btn7.Text = ""
+        End If
+        If btn6.Text = "" Then
+            btn6.Text = btn7.Text
+            btn7.Text = ""
+        End If
+        If btn8.Text = "" Then
+            btn8.Text = btn7.Text
+            btn7.Text = ""
+        End If
+        If btn11.Text = "" Then
+            btn11.Text = btn7.Text
+            btn7.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        If btn4.Text = "" Then
+            btn4.Text = btn8.Text
+            btn8.Text = ""
+        End If
+        If btn7.Text = "" Then
+            btn7.Text = btn8.Text
+            btn8.Text = ""
+        End If
+        If btn12.Text = "" Then
+            btn12.Text = btn8.Text
+            btn8.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        If btn5.Text = "" Then
+            btn5.Text = btn9.Text
+            btn9.Text = ""
+        End If
+        If btn10.Text = "" Then
+            btn10.Text = btn9.Text
+            btn9.Text = ""
+        End If
+        If btn13.Text = "" Then
+            btn13.Text = btn9.Text
+            btn9.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn10_Click(sender As Object, e As EventArgs) Handles btn10.Click
+        If btn6.Text = "" Then
+            btn6.Text = btn10.Text
+            btn10.Text = ""
+        End If
+        If btn9.Text = "" Then
+            btn9.Text = btn10.Text
+            btn10.Text = ""
+        End If
+        If btn11.Text = "" Then
+            btn11.Text = btn10.Text
+            btn10.Text = ""
+        End If
+        If btn14.Text = "" Then
+            btn14.Text = btn10.Text
+            btn10.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn11_Click(sender As Object, e As EventArgs) Handles btn11.Click
+        If btn7.Text = "" Then
+            btn7.Text = btn11.Text
+            btn11.Text = ""
+        End If
+        If btn10.Text = "" Then
+            btn10.Text = btn11.Text
+            btn11.Text = ""
+        End If
+        If btn12.Text = "" Then
+            btn12.Text = btn11.Text
+            btn11.Text = ""
+        End If
+        If btn15.Text = "" Then
+            btn15.Text = btn11.Text
+            btn11.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn12_Click(sender As Object, e As EventArgs) Handles btn12.Click
+        If btn8.Text = "" Then
+            btn8.Text = btn12.Text
+            btn12.Text = ""
+        End If
+        If btn11.Text = "" Then
+            btn11.Text = btn12.Text
+            btn12.Text = ""
+        End If
+        If btn16.Text = "" Then
+            btn16.Text = btn12.Text
+            btn12.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn13_Click(sender As Object, e As EventArgs) Handles btn13.Click
+        If btn9.Text = "" Then
+            btn9.Text = btn13.Text
+            btn13.Text = ""
+        End If
+        If btn14.Text = "" Then
+            btn14.Text = btn13.Text
+            btn13.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn14_Click(sender As Object, e As EventArgs) Handles btn14.Click
+        If btn10.Text = "" Then
+            btn10.Text = btn14.Text
+            btn14.Text = ""
+        End If
+        If btn15.Text = "" Then
+            btn15.Text = btn14.Text
+            btn14.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn15_Click(sender As Object, e As EventArgs) Handles btn15.Click
+        If btn14.Text = "" Then
+            btn14.Text = btn15.Text
+            btn15.Text = ""
+        End If
+        If btn16.Text = "" Then
+            btn16.Text = btn15.Text
+            btn15.Text = ""
+        End If
+        If btn11.Text = "" Then
+            btn11.Text = btn15.Text
+            btn15.Text = ""
+        End If
+    End Sub
+
+    Private Sub btn16_Click(sender As Object, e As EventArgs) Handles btn16.Click
+        If btn12.Text = "" Then
+            btn12.Text = btn16.Text
+            btn16.Text = ""
+        End If
+        If btn15.Text = "" Then
+            btn15.Text = btn16.Text
+            btn16.Text = ""
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        btn1.Text = 2
+        btn2.Text = 4
+        btn3.Text = 1
+        btn4.Text = 3
+        btn5.Text = 6
+        btn6.Text = 5
+        btn7.Text = 9
+        btn8.Text = 7
+        btn9.Text = ""
+        btn10.Text = 15
+        btn11.Text = 14
+        btn12.Text = 11
+        btn13.Text = 10
+        btn14.Text = 12
+        btn15.Text = 8
+        btn16.Text = 13
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        If btn1.Text = "1" And btn2.Text = "2" And btn3.Text = "3" And btn4.Text = "4" And btn5.Text = "5" And btn6.Text = "6" And btn7.Text = "7" And btn8.Text = "8" And btn9.Text = "9" And btn10.Text = "10" And btn11.Text = "11" And btn12.Text = "12" And btn13.Text = "13" And btn14.Text = "14" And btn15.Text = "15" And btn16.Text = "" Then
+        Else
+            MsgBox("Done")
+        End If
+    End Sub
+End Class
